b332c08bd15db633303161a296b3f5d2a427b290
dimadovzenok <dima.dovzenok2003@gmail.com>
2021-12-07 12:09:53 +0200

    Req1 - get 100 number - fail

--- src/fizzbuzzkataDovzenok/fizzbuzzkata.java
+++ src/fizzbuzzkataDovzenok/fizzbuzzkata.java
@@ -1,5 +1,15 @@
 package fizzbuzzkataDovzenok;
 
 public class fizzbuzzkata {
+	
+	private int[] numbers;
+
+	public int[] getNumbers() {
+		numbers = new int[100];
+		for(int i = 0; i< numbers.length; i++) {
+			numbers[i]=i+1;
+		}
+		return numbers;
+	}
 
 }
