4c5f50fb788b4ade76332e2a985a56a83df432a6
dimadovzenok <dima.dovzenok2003@gmail.com>
2021-12-07 12:11:04 +0200

    Req1 - get 100 number - pass

--- src/fizzbuzzkataDovzenok/fizzbuzzkataTest.java
+++ src/fizzbuzzkataDovzenok/fizzbuzzkataTest.java
@@ -2,18 +2,13 @@ package fizzbuzzkataDovzenok;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class fizzbuzzkataTest {
 
-	@BeforeEach
-	public void setUp() throws Exception {
-		fizzbuzzkata fizz = new fizzbuzzkata();
-	}
-
 	@Test
 	public void Return100() {
+		fizzbuzzkata fizz = new fizzbuzzkata();
 		assertEquals(100, fizz.getNumbers().length);
 	}
 
