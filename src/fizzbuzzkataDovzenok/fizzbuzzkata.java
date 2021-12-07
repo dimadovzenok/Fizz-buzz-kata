4e8cdbc343660ce328f7ca3d9c2b2d64cbed491c
dimadovzenok <dima.dovzenok2003@gmail.com>
2021-12-07 12:27:52 +0200

    Req5 - get FizzBuzz for 15 - pass

--- src/fizzbuzzkataDovzenok/fizzbuzzkata.java
+++ src/fizzbuzzkataDovzenok/fizzbuzzkata.java
@@ -15,6 +15,8 @@ public class fizzbuzzkata {
 	public String fizbuzkat(int val) {
 		if(val == 0){
 			return Integer.toString(val);
+		}else if (val % 3 == 0 && val % 5 == 0) {
+			return "FizzBuzz";
 		}else if (val % 3 == 0) {
 			return "Fizz";
 		}else if (val % 5 == 0) {
